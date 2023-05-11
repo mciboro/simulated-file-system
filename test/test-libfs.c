5530aec7432d98f82074af4f2ab62d7428646ee0
Minh Nguyen Cong <congminh1254@gmail.com>
2023-05-12 01:32:05 +0200

    Setup tests

---
+++ test/test-libfs.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "ctest.h"
+
+// basic test without setup/teardown
+CTEST(suite1, test1) {
+}
+
+// there are many different ASSERT macro's (see ctest.h)
+CTEST(suite1, test2) {
+    ASSERT_EQUAL(1,1);
+}
+
+CTEST(suite2, test1) {
+    ASSERT_STR("foo", "foo");
+}
+
+CTEST(suite3, test3) {
+    ASSERT_EQUAL(1,2);
+}
